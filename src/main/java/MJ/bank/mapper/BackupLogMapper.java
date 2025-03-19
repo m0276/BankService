3fa6db5befa862a2ee46e04ef68e3f2949694c36
LeeMinJu <mmnnii0276@gmail.com>
2025-03-19 14:34:11 +0900

    fix : fix update log mapper

--- src/main/java/MJ/bank/mapper/BackupLogMapper.java
+++ src/main/java/MJ/bank/mapper/BackupLogMapper.java
@@ -3,12 +3,6 @@ package MJ.bank.mapper;
 
 import MJ.bank.dto.BackupDto;
 import MJ.bank.entity.BackupLog;
-import MJ.bank.entity.BackupStatus;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import java.time.LocalDateTime;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
--- src/main/java/MJ/bank/mapper/UpdateLogMapper.java
+++ src/main/java/MJ/bank/mapper/UpdateLogMapper.java
@@ -1,6 +1,7 @@
 package MJ.bank.mapper;
 
 
+import MJ.bank.dto.UpdateDiffDto;
 import MJ.bank.dto.UpdateLogDto;
 import MJ.bank.entity.UpdateLog;
 import org.mapstruct.Mapper;
@@ -9,7 +10,17 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring", uses = {UpdateLogMapperHelper.class})
 public interface UpdateLogMapper {
 
+    @Mapping(source = "propertyName", target = "propertyName", ignore = true)
+    @Mapping(source = "currContent", target = "currContent", ignore = true)
+    @Mapping(source = "prevContent", target = "prevContent", ignore = true)
     @Mapping(source = "employeeId", target = "employeeNumber", qualifiedByName = "LongToString")
     public UpdateLogDto toDto(UpdateLog updateLog);
+
+
+    @Mapping(source = "employeeId", target = "employeeNumber", ignore = true)
+    @Mapping(source = "memo", target = "memo", ignore = true)
+    @Mapping(source = "ip", target = "ip", ignore = true)
+    @Mapping(source = "updateTime", target = "updateTime", ignore = true)
+    public UpdateDiffDto toDiffDto(UpdateLog updateLog);
 
 }
