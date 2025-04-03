607250b3211eb5141e9ea78cd9f209481b688cf2
LeeMinJu <mmnnii0276@gmail.com>
2025-04-03 18:31:25 +0900

    feat:add dashboard controller method

--- src/main/java/MJ/bank/controller/DashboardController.java
+++ src/main/java/MJ/bank/controller/DashboardController.java
@@ -1,9 +1,15 @@
 package MJ.bank.controller;
 
 
+import MJ.bank.dto.DashboardDto;
+import MJ.bank.dto.response.ErrorResponse;
+import MJ.bank.service.DashboardService;
 import MJ.bank.service.EmployeeService;
 import MJ.bank.service.UpdateLogService;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,4 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/dashboard")
 public class DashboardController {
 
+  private final DashboardService dashboardService;
+
+  @GetMapping
+  public ResponseEntity<?> get(){
+    try{
+      return ResponseEntity.status(HttpStatus.OK).body(dashboardService.getDashboard());
+    } catch (Exception e){
+     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(
+         LocalDateTime.now(),
+         HttpStatus.INTERNAL_SERVER_ERROR.value(),
+         "서버 오류입니다.",
+         "예기치 못한 오류가 발생했습니다."
+     ));
+    }
+  }
 }
